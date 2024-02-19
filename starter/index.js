50aa14182c8026518a888477ae15d48bebbd0e21
HarunaTech <hamodharuna@gmail.com>
2024-02-20 01:26:28 +0200

    updated prompt question for user

--- starter/index.js
+++ starter/index.js
@@ -20,10 +20,41 @@ function init() {
         name: "Title",
         message: "What is the title of the project? ",
       },
+      {
+        name: "Description",
+        message: "What is the description of the project? ",
+      },
+      {
+        name: "Installation",
+        message: "What is the installation of the project? ",
+      },
+      {
+        name: "Usage",
+        message: "What is the usage information of the project? ",
+      },
+      {
+        name: "Contributing",
+        message: "What is the contribution guildlines? ",
+      },
+      {
+        name: "Tests",
+        message: "Please provide testing instructions? ",
+      },
+      {
+        type: "checkbox",
+        name: "License",
+        message: "What type of License does the project has? ",
+        choices: [
+            'Apache License 2.0', 'MIT', 'GPL-3.0', 'BSD-2-Clause',
+          ],
+      },
     ])
     .then((answers) => {
       console.info("Answer:", answers.Title);
-      console.log(generateMarkdown(answers));
+      console.info("Answer:", answers.Description);
+      console.info("Answer:", answers.Installation);
+
+      console.log(generateMarkdown(answers));  // This is consologing to see how it's display according to the md tempplate 
     });
 
 }
--- starter/utils/generateMarkdown.js
+++ starter/utils/generateMarkdown.js
@@ -1,7 +1,9 @@
-// function to generate markdown for README
+// function to generate markdown for README 
+// # indicate Heading 1 style according to readme markdown and Title is relative to index.js prompt
 function generateMarkdown(data) {
-  return `# ${data.Title}
-
+  return `# ${data.Title}  
+  
+  
 `;
 }
 
